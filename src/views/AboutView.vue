4a7bd6c2b2e407c5a75a34716c42dbf4d3a0eb96
lucifer3049 <81276617+lucifer3049@users.noreply.github.com>
2023-08-23 22:53:07 +0800

    git add

---
+++ src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      data: {},
+    }
+  },
+  mounted() {
+    console.log(import.meta.env.VITE_TEXT);
+    const url = import.meta.env.VITE_PATH;  //APi寫成環境變數
+    this.$http.get(url)
+      .then((res) => {
+        console.log(res.data);
+        this.data = res.data.results[0]
+        console.log(res.data.results)
+      })
+  }
+}
+
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
